ec348c944c79f2dd18e76fb28238355a5db81e54
Amanda Guerriero <ag.codingclass@gmail.com>
2020-08-22 16:51:06 -0700

    Updated skip prompts and shop cases

--- assets/js/game.js
+++ assets/js/game.js
@@ -110,18 +110,16 @@ var startGame = function () {
 var shop = function () {
     console.log("Entered the Shop");
     // Ask player what they would like to do?
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "refill": // new case
-        case "REFILL":
+        case 1:
             playerInfo.refillHealth();
             break; 
-        case "UPGRADE": // new case
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             // Do nothing, end function
             break;
