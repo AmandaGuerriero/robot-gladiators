322a9c6c869bd520a5104cbda015660242b18a7f
Amanda Guerriero <ag.codingclass@gmail.com>
2020-08-22 17:08:08 -0700

    Randomized attacks

--- assets/js/game.js
+++ assets/js/game.js
@@ -43,28 +43,34 @@ var fightOrSkip = function(){
 
 var fight = function(enemy) {
     console.log(enemy);
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
     while (playerInfo.health > 0 && enemy.health > 0)  {
-        if (fightOrSkip ()) {
-            break;
-        }
-        else {
+        if (isPlayerTurn) {
+            if (fightOrSkip ()) {
+                break;
+            }
             var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
                 enemy.health = Math.max(0, enemy.health - damage);
             console.log(
-                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining. ");
-        }
+            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining. ");
+
             // check enemy's health
             if (enemy.health <= 0) {
                 window.alert(enemy.name + " has died! ");
+                playerInfo.money = playerInfo.money + 20;
                 break;
             } else {
                 window.alert(enemy.name + " still has " + enemy.health + " health left. ");
             }
-
+            // Player attacked first
+        } else {
             // Update playerInfo.health by subtracting enemy.attack
             // playerHealth = Math.max(0, playerHealth - enemy.attack);
             var damage = randomNumber(enemy.attack - 3, enemy.attack);
-                playerInfo.health = Math.max(0, playerInfo.health - damage);            
+            playerInfo.health = Math.max(0, playerInfo.health - damage);            
             console.log(
                 enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining. ");
 
@@ -76,8 +82,9 @@ var fight = function(enemy) {
                 window.alert (playerInfo.name + " still has " + playerInfo.health + " health left. ");
             }
         }
+        isPlayerTurn = !isPlayerTurn;
     }
-
+}
 
 var startGame = function () {
     // Reset Player Health
